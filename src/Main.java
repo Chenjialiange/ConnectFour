8d9f705abc0f598451a688a721f057027369d044
Jeffrey <348815291jeffrey@gmail.com>
2025-06-10 20:27:49 -0400

    first sub

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        char[][] gameboard = new char[7][6];
+        Boolean Xturn = true;
+        Boolean win = false;
+        while (win == false){
+            if (Xturn == true){
+                System.out.println("X Move, Column:");
+                int col = sc.nextInt();
+                for (int i = 0; i<6; i++){
+                    if (gameboard[col][i] == ('X') || gameboard[col][i] == 'Y'){
+                        continue;
+                    } else{
+                        gameboard[col][i] = 'X';
+                    }
+                }
+                for (int i = 0; i<7; i++){
+                    for (int j  = 0; i<6; j++){
+                        if(gameboard[i][j] == 'X'){
+                            System.out.print("[X]");
+                        } if(gameboard[i][j] == 'Y'){
+                            System.out.print("[Y]");
+                        }else{
+                            System.out.print("[]");
+                        }
+                        System.out.println();
+                    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+                }
+            }
+
+
         }
     }
 }
